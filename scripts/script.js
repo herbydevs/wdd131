838449f7279a1a3741a3ab385050948230c99ae4
herbydevs <yme7ir@gmail.com>
2026-01-27 04:52:07 -0400

    week 3 project

--- scripts/script.js
+++ scripts/script.js
@@ -4,22 +4,19 @@ document.querySelector("#year").textContent = currentYear;
 const lastModified = document.lastModified;
 document.querySelector("#lastModified").textContent = `Last Modification: ${lastModified}`;
 
-const temp = 27;
-const windSpeed = 15;
+const temp = 8; // Lowered temp to test the wind chill logic
+const windSpeed = 10;
 
 document.querySelector("#temp").innerHTML = `${temp}&deg;C`;
 document.querySelector("#wind").innerHTML = `${windSpeed} km/h`;
 
-function calculateWindChill(t, s) {
-    return 13.12 + (0.6215 * t) - (11.37 * Math.pow(s, 0.16)) + (0.3965 * t * Math.pow(s, 0.16));
-}
+// Criteria 7: One-line function
+const calculateWindChill = (t, s) => 13.12 + (0.6215 * t) - (11.37 * Math.pow(s, 0.16)) + (0.3965 * t * Math.pow(s, 0.16));
 
 let windChill = "N/A";
 
 if (temp <= 10 && windSpeed > 4.8) {
-    windChill = calculateWindChill(temp, windSpeed).toFixed(1) + "&deg;C";
-} else {
-    windChill = "N/A";
+    windChill = `${calculateWindChill(temp, windSpeed).toFixed(1)}&deg;C`;
 }
 
 document.querySelector("#chill").innerHTML = windChill;
